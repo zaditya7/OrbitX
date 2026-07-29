f508d74aac281ad5faccbd4b97577ecc579cb2ef
Aditya <zadityasahani@gmail.com>
2026-07-30 02:21:59 +0530

    add dashboard css

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,17 @@
+import StatCard from "./StatCard";
+import "./Dashboard.css";
+
 function Dashboard() {
     return(
         <main>
             <h1>Dashboard</h1>
             <p>Welcome to Orbitx mission control.</p>
+            <div className="cards">
+                <StatCard />
+                <StatCard />
+                <StatCard />
+                <StatCard />
+            </div>
         </main>
     );
 }
---
+++ frontend/src/components/StatCard.jsx
@@ -0,0 +1,11 @@
+import "./StatCard.css";
+
+function StatCard() {
+    return(
+        <div className="stat-card">
+            <h3>Total Satellites</h3>
+            <h1>18</h1>
+        </div>
+    );
+}
+export default StatCard;
