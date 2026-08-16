f96506add40ef929770164a40c6bced7ee489f4c
Aditya <zadityasahani@gmail.com>
2026-08-16 07:20:51 +0530

    making earth digital

--- frontend/src/components/MissionGlobe.jsx
+++ frontend/src/components/MissionGlobe.jsx
@@ -6,11 +6,15 @@ function MissionGlobe() {
       <Globe
         width={900}
         height={600}
-        backgroundColor="#050b18"
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-        atmosphereColor="#4da6ff"
-        atmosphereAltitude={0.15}
+        backgroundColor="#020817"
+        globeMaterial={{
+          color: "#071a2e",
+          emissive: "#03101f",
+          emissiveIntensity: 0.6,
+          shininess: 0.8
+        }}
+        atmosphereColor="#00aaff"
+        atmosphereAltitude={0.12}
         enablePointerInteraction={true}
       />
     </div>
